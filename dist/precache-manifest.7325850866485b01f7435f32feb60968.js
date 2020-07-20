f458ba23568de869727eaf41064e8df8b3d62b43
Romaine Murray <romainemurray6@gmail.com>
2020-07-20 09:40:16 -0500

    fixed styling

---
+++ dist/precache-manifest.7325850866485b01f7435f32feb60968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f09e09581906d263f7",
+    "url": "/assets/css/app.8154a755.css"
+  },
+  {
+    "revision": "7e2f750c3fa6f8e7240e017f2fae0113",
+    "url": "/assets/img/background.7e2f750c.png"
+  },
+  {
+    "revision": "d6f09e09581906d263f7",
+    "url": "/assets/js/app.9a5687e0.js"
+  },
+  {
+    "revision": "8b422249831ee793d51d",
+    "url": "/assets/js/chunk-vendors.e4db1547.js"
+  },
+  {
+    "revision": "7ace45bb807b393606736d7f31ee2208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "148d5a9a30cdad94ff49ec8cc7a2d7f2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="flex flex-col min-h-screen">
-    <router-view class="flex-grow mx-auto p-8" />
+    <router-view class="mx-auto my-auto p-8" />
     <Footer></Footer>
   </div>
 </template>
