a40f839318297899f54cddb460c80b176625e8bc
Romaine Murray <romaine.murray@mymona.uwi.edu>
2020-10-25 20:42:25 -0500

    fixed some css

--- src/components/Capital.vue
+++ src/components/Capital.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <p class="font-extrabold text-blue-900 pb-4 text-base1 mr-6">
+    <p class="font-bold text-blue-900 pb-4 text-base1 mr-6">
       {{ question }}
     </p>
     <div class="mx-0">
       <p
         v-for="(answer, index) in answers"
         :key="index"
-        class=" text-sm border border-solid rounded-lg p-1 px-18 pl-3 m-2 mx-0 mb-5 cursor-pointer hover:text-white hover:border-opacity-0 text-purple-500 hover:bg-orange-500 border-purple-500 flex flex-row"
+        class=" text-sm border-2 border-solid rounded-lg p-1 px-18 pl-3 m-2 mx-0 mb-5 cursor-pointer hover:text-white text-purple-500 hover:bg-orange-500 border-purple-500 flex flex-row"
         :class="{
           correct: answer.isCorrect && pickedAnswer,
           incorrect: !answer.isCorrect && pickedAnswer === answer,
@@ -16,7 +16,7 @@
         }"
         @click="selectAnswer(answer)"
       >
-        <span class="pr-4 text-lg font-bold">{{ answerLetters[index] }}</span>
+        <span class="pr-4 text-lg font-medium">{{ answerLetters[index] }}</span>
         <span class="flex-grow mt-3px">{{ answer.Ans }}</span>
         <CheckCircleOutline
           v-if="answer.isCorrect && pickedAnswer"
